f2ec45a134f9e7278a2321db20dc6ee41f7583d4
Anton Gladyshev <returnstrike@yandex.com>
2025-09-09 14:29:41 +0300

    added last_executed

--- gt_stats.py
+++ gt_stats.py
@@ -103,7 +103,7 @@ def get_obj_stats (cluster, db, table):
 
         # Debug: print executed query
         if True:
-            if hasattr(db_con.cur, "_last_executed"):   # MySQL
+            if hasattr(db_con.cur, "statement"):   # MySQL
                 print("Executed query:", db_con.cur.statement)
             else:
                 print("Executed query text unavailable (no attribute for this driver)")
